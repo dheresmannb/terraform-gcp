c357b4efe7cfd6688957e7d51265780d6a009053
Daniel Heresmann <daniel.heresmann@devaid.cl>
2022-10-14 01:08:34 -0300

    fix

--- terraform-gcp-create-storage-bucket/main.tf
+++ terraform-gcp-create-storage-bucket/main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "create_storage_bucket" {
   name     = var.name
   location = var.location
-  project_id = var.project_id
+  project  = var.project_id
   labels   = var.labels
 }
